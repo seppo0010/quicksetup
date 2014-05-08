57cdacb2f0fb4a91d9e80e600fff81daafb7bc33
Sebastian Waisbrot <seppo@Seppos-MacBook-Air-2.local>
2014-05-08 05:20:34 -0700

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y vim zsh curl git-core
+
+echo "EDITOR=vim" >> ~/.bashrc
+
+curl -L http://install.ohmyz.sh | sh
+
+sudo chsh -s /bin/zsh `whoami`
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="tonotdo"/' ~/.zshrc
+
+git clone https://github.com/mariano/dot-vim ~/.vim
+
+pushd ~/.vim
+git submodule update --init
+cd ..
+ln -s .vim/.vimrc
+popd
+
+cat >> ~/.vimrc <<EOF
+function! ThreeSpaces()
+    set expandtab▸  ▸   " Treat TAB as spacesa
+    set tabstop=3▸  ▸   " TAB is actually 3 spaces
+    set shiftwidth=3▸   " Doing >> on a block whill shift it one tab (based on ts setting above)
+    set softtabstop=3   " makes the spaces feel like real tabs
+endfunction
+
+function! TwoSpaces()
+    set expandtab▸  ▸   " Treat TAB as spacesa
+    set tabstop=2▸  ▸   " TAB is actually 2 spaces
+    set shiftwidth=2▸   " Doing >> on a block whill shift it one tab (based on ts setting above)
+    set softtabstop=2   " makes the spaces feel like real tabs
+endfunction
+
+:call ThreeSpaces()
+EOF
+
+unlink ~/.vim/bundle/jslint
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st "status --short"
+git config --global alias.br branch
+git config --global core.excludesfile ~/.gitignore_global
+git config --global alias.fix "commit --amend -C HEAD"
+
+git config --global user.name "Sebastian Waisbrot"
+git config --global user.email "seppo0010@gmail.com"
+
+git config --global push.default simple
+
+echo ".*.swp" > ~/.gitignore_global
